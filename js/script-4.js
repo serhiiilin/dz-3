7dd45ca6a98f0650d91bc08bf36c62295dd6c8f5
Sergey <rediska668@gmail.com>
2022-01-03 16:20:43 +0200

    03.01-16:20

---
+++ js/script-4.js
@@ -0,0 +1,34 @@
+'use script';
+
+
+const products = [
+    {
+    id: 3,
+    price: 127,
+    photos: [
+    "1.jpg",
+    "2.jpg",
+    ]
+    },
+    {
+    id: 5,
+    price: 499,
+    photos: []
+    },
+    {
+    id: 10,
+    price: 26,
+    photos: [
+    "3.jpg"
+    ]
+    },
+    {
+    id: 8,
+    price: 78,
+    },
+    ];
+    
+    let productsPhoto = products.filter(function(photo){
+        return photo.photos == "" && photo.photos =='1.jpg';
+    })
+    console.log(productsPhoto);
